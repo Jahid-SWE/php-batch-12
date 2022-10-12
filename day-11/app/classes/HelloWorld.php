275d789490c84584a6362b0ba9cdcf811b40d748
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2022-10-12 15:45:58 +0600

    Initial commit

---
+++ day-11/app/classes/HelloWorld.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Classes;
+
+class HelloWorld{
+
+    public $text;
+    public function __construct()
+    {
+        $this->text = "hello World";
+        
+    } 
+    public function one(){
+        echo " I love My Mother";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ day-11/index.php
@@ -0,0 +1,28 @@
+<?php
+require_once "vendor/autoload.php";
+use App\Classes\HelloWorld;
+
+$student= new HelloWorld();
+$student->one();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
